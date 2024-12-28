a7dce5a165073b8ae24eced0e91c6d6c24589f7c
shanmukha <shanmukhaagnihotram@gmail.com>
2024-12-28 21:03:02 +0530

    shannu commited now

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+      <p>© 2024 TORTO-FORTO. All Rights Reserved.</p>
+      <p>Contact us: support@torto-forto.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
